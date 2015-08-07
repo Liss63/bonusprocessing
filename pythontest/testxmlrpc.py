c4698e6a5a0e50ffea0045a1ffe472e4996d1ecc
slipatov <s.lipatov@mail.ru>
2015-08-07 10:22:29 +0400

    add profiler class

--- pythontest/testxmlrpc.py
+++ pythontest/testxmlrpc.py
@@ -1,11 +1,20 @@
 from xmlrpclib import ServerProxy
+import time
+
+class Profiler(object):
+    def __enter__(self):
+        self._startTime = time.time()
+
+    def __exit__(self, type, value, traceback):
+        print "Elapsed time: {:.3f} sec".format(time.time() - self._startTime)
 
 if __name__ == "__main__":
     s = ServerProxy("http://localhost:7080")
     print s.add(1, 2)
     print s.echo("hello")
     print s.AddCard("34224", 0)
-    print s.SetBalance("34224", 5)
+    with Profiler() as p:
+        print s.SetBalance("34224", 5)
     print s.GetBalance("34224")
     print s.IncBalance("34224", 2)
     print s.DecBalance("34224", 1)
